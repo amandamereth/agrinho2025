cb8b940bb3c95c2e517ac028f2636c6a160b7d2d
amandamereth <amanda.mereth.goncalves@escola.pr.gov.br>
2025-05-29 11:20:23 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+let jogadorA = 65, jogadorB = 265, jogadorC = 60;
+let obstaculos = [];
+let velocidadeObstaculo = 6;
+let jogoAtivo = true;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 460);
 }
 
 function draw() {
-  background(220);
+  background("yellow");
+
+  // Desenha o jogador
+  fill(0, 0, "white");
+  rect(jogadorA, jogadorB, jogadorC, jogadorC);
+
+  // Movimento do jogador
+  if (keyIsDown(UP_ARROW) && jogadorB > 0) jogadorB -= 7;
+  if (keyIsDown(DOWN_ARROW) && jogadorB < height - jogadorC) jogadorC += 7;
+
+  // Adiciona obstáculos
+  if (frameCount % 75 === 0 && jogoAtivo) {
+    let oX = width;
+    let oY = random(0, height - 50);
+    obstaculos.push({ x: oX, y: oY });
+  }
+
 }
